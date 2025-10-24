9b616652c83542e8a686831d63b96892c18c5cdd
Сева <vopopenko@gmail.com>
2025-10-24 18:59:48 +0300

    add "UseStaticFiles"

--- Server/Program.cs
+++ Server/Program.cs
@@ -19,6 +19,8 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
 var app = builder.Build();
 app.UseAuthorization();
 app.UseAuthentication();
+app.UseDefaultFiles();
+app.UseStaticFiles();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller}/{action}"
