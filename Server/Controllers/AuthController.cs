be3dc6d36a2e49648b27cfa0064d57f52d45be24
Leonid Samoshenko <samoshenkoleonid@yandex.ru>
2025-10-24 19:05:28 +0300

    confict

--- Server/Controllers/AuthController.cs
+++ Server/Controllers/AuthController.cs
@@ -1,7 +1,8 @@
-﻿
+﻿using Server.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Localization;
 using Microsoft.AspNetCore.SignalR;
+using System.ComponentModel.DataAnnotations;
 
 namespace SignalRApp
 {
