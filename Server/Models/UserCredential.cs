3128ab30e0a6ea2fa8c9a11722784c97ce6a8d2d
Boris <93731664+hikkahikka@users.noreply.github.com>
2025-10-24 17:05:42 +0300

    Add UserCredential

---
+++ Server/Models/UserCredential.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Server.Models
+{
+    public class UserCredential
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
