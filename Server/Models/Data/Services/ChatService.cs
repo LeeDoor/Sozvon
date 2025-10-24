2b65d70756f7fb158f7d71400d6f44450302719c
Boris <93731664+hikkahikka@users.noreply.github.com>
2025-10-25 02:06:59 +0300

    Add ChatServise

---
+++ Server/Models/Data/Services/ChatService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Server.Models.Data.Services
+{
+    public class ChatService
+    {
+        private readonly ApplicationContext _context;
+
+        public ChatService(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ChatMessage> SendMessageAsync(ConferenceRoomId roomId, UserId userId, string content)
+        {
+            var message = new ChatMessage
+            {
+                ConferenceRoomId = roomId,
+                UserId = userId,
+                Content = content,
+                DateTime = DateTime.UtcNow
+            };
+
+            _context.ChatMessages.Add(message);
+            await _context.SaveChangesAsync();
+            return message;
+        }
+
+        public async Task<List<ChatMessage>> GetRoomMessagesAsync(ConferenceRoomId roomId, int count = 100)
+        {
+            return await _context.ChatMessages
+                .Where(m => m.ConferenceRoomId == roomId)
+                .Include(m => m.User)
+                .OrderByDescending(m => m.DateTime)
+                .Take(count)
+                .OrderBy(m => m.DateTime) 
+                .ToListAsync();
+        }
+    }
+}
