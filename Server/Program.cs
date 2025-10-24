b1fc064a286e3e344491643c4061795f37141c2d
Shapik <egorshapovalov06@gmail.com>
2025-10-24 18:22:29 +0300

    add index.html, change Register, Login

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,9 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 var app = builder.Build();
+app.UseDefaultFiles();
+app.UseStaticFiles();
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Auth}/{action=Register}"
+    pattern: "{controller}/{action}"
 );
 
 app.Run();
