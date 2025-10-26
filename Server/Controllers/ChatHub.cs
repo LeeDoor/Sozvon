d415ee18b9112742017bca6ad683aa438982205d
Boris <93731664+hikkahikka@users.noreply.github.com>
2025-10-26 14:52:52 +0300

    add ban words

--- Server/Controllers/ChatHub.cs
+++ Server/Controllers/ChatHub.cs
@@ -6,9 +6,32 @@ namespace SignalRApp
     {
         public async Task Send(string message)
         {
-            await Clients.All.SendAsync("Receive", message, Context?.User?.Identity?.Name ?? "Гость");
+            if (ContainsBadWords(message)) await Clients.All.SendAsync("Receive", message, Context?.User?.Identity?.Name ?? "Гость");
+            else await Clients.Caller.SendAsync("BadMessage");//напомнить лене обработать
         }
-    public override async Task OnConnectedAsync()
+        private bool ContainsBadWords(string message)
+        {
+            string projectDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+            string filePath = Path.Combine(projectDirectory, "banWords.txt");
+
+            if (string.IsNullOrEmpty(message))
+                return false;
+
+            var banWords = File.ReadAllLines(filePath)
+                .Where(line => !string.IsNullOrWhiteSpace(line))
+                .Select(line => line.Trim().ToLower());
+
+            foreach (var banWord in banWords)
+            {
+                if (message.ToLower().Contains(banWord))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        public override async Task OnConnectedAsync()
         {
             await Clients.All.SendAsync("Notify", $"{Context.ConnectionId} вошел в чат");
             await base.OnConnectedAsync();
